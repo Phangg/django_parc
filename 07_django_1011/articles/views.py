2d9aa930c97f271211e8c06f63e83e68df182cf5
Phangg <leeckdwns9305@gmail.com>
2022-10-14 11:38:38 +0900

    1014

--- 07_django_1011/articles/views.py
+++ 07_django_1011/articles/views.py
@@ -52,7 +52,7 @@ def delete(request, pk):
             article = Article.objects.get(pk=pk)
             article.delete()
             return redirect('articles:index')
-    return redirect('articles:detail', article.pk)
+    return redirect('articles:detail', pk)
 
 
 @login_required
